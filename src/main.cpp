98e80d17b55b07a9efcda86090be49971ec9f062
MaciekUXF <maciek@uxfactor.pl>
2022-09-01 23:36:16 +0200

    MVP

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include "BluetoothA2DPSink.h"
+
+BluetoothA2DPSink a2dp_sink;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  static i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
+      .sample_rate = 44100, // updated automatically by A2DP
+      .bits_per_sample = (i2s_bits_per_sample_t)32,
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+      .intr_alloc_flags = 0, // default interrupt priority
+      .dma_buf_count = 8,
+      .dma_buf_len = 64,
+      .use_apll = true,
+      .tx_desc_auto_clear = true // avoiding noise in case of data unavailability
+  };
+
+  a2dp_sink.set_i2s_config(i2s_config);
+  a2dp_sink.start("MollyMusic", true);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
